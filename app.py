c7908cec5b070769b4c061bd5a7f6b6f22ca00d9
varshadaavhad-cpu <varshadaavhad@gmail.com>
2026-05-08 14:45:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,6 +149,11 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
